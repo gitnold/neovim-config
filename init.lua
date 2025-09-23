ba6d4b5b758bfbab195764ab806b8edb77f47c26
Arnold <mutwirinold2@gmail.com>
2025-09-23 21:48:02 +0300

    Track neovim config

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Set transparent background in Neovim
+-- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi SignColumn guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi VertSplit guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi StatusLine guibg=NONE ctermbg=NONE")
+-- vim.cmd("hi StatusLineNC guibg=NONE ctermbg=NONE")
---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options before
+--
+
+local opt = vim.opt
+
+vim.g.autoformat = false
+opt.ruler = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+
